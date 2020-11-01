62d8d977e30c9ec8faad03ded7376cf9a392218f
Brian Rocca <brocca@iu.edu>
2020-11-01 08:21:55 -0500

    update

--- practicals/transcribe.py
+++ practicals/transcribe.py
@@ -37,7 +37,7 @@ for c in str:
 	print('# sent_id = %d' % (line))
 	print('# text = %s' % (c))
 	counter = 0
-	lista = c.replace('.', ' .').replace(',', ' , ').replace('?', ' ?').replace('!', ' !').replace(';', ' ;').replace(':', ' :').replace(')', ' )').replace('(', '( ').replace(' "', '" ').replace('" ', ' " ').replace('\n', '').replace('"', ' " ').replace('/', ' / ')
+	lista = c.replace('.', ' .').replace(',', ' , ').replace('?', ' ?').replace('!', ' !').replace(';', ' ;').replace(':', ' :').replace(')', ' )').replace('(', '( ').replace(' "', '" ').replace('" ', ' " ').replace('\n', '').replace('"', ' " ').replace('/', ' / ').replace('  ', ' ')
 	lista = lista.split(' ')
 	for token in lista:
 		counter = counter + 1
